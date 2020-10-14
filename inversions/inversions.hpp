02d8204510ad9128697615f2e9ceef21dc303da3
Dylan Palmieri <dgpalmieri@gmail.com>
2020-10-14 15:01:02 -0800

    Add mock inversions.hpp and provided tests

---
+++ inversions/inversions.hpp
@@ -0,0 +1,16 @@
+// inversions.hpp
+// Dylan Palmieri
+// 2020-10-14
+// Header containing inversions template function
+
+#ifndef CONTIGSUM_HPP
+#define CONTIGSUM_HPP
+
+#include<cstddef>
+
+template<typename RAIter>
+std::size_t inversions(RAIter first, RAIter last){
+    return 0;
+}
+
+#endif /* CONTIGSUM_HPP */
