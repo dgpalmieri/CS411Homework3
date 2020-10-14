799bd0423fbcdd21d48322bafbefa5cdbc20bc51
Dylan Palmieri <dgpalmieri@gmail.com>
2020-10-14 15:00:25 -0800

    Add mock contigsum.hpp and provided tests

---
+++ contigsum/contigsum.hpp
@@ -0,0 +1,14 @@
+// contigsum.hpp
+// Dylan Palmieri
+// 2020-10-14
+// Header containing contiguous subset sum template function
+
+#ifndef CONTIGSUM_HPP
+#define CONTIGSUM_HPP
+
+template<typename RAIter>
+int contigSum(RAIter first, RAIter last){
+    return 0;
+}
+
+#endif /* CONTIGSUM_HPP */
