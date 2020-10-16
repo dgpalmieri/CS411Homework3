7957f8f46cd069a7726f252d2f4aba27f021d14d
Dylan Palmieri <dgpalmieri@gmail.com>
2020-10-15 23:48:34 -0800

    Add merge_sort.cpp functionality to inversions.hpp

--- inversions/inversions.hpp
+++ inversions/inversions.hpp
@@ -6,10 +6,107 @@
 #ifndef CONTIGSUM_HPP
 #define CONTIGSUM_HPP
 
-#include<cstddef>
+// ********************* FROM merge_sort.cpp, by G.G. Chappell *********************
+
+#include <iostream>
+#include <vector>
+#include <cstddef>
+#include <algorithm>
+#include <iterator>
+#include <type_traits>
+#include<iterator>
+
+
+// stableMerge
+// Merge two halves of a sequence, each sorted, into a single sorted
+// sequence in the same location. Merge is done in a stable manner.
+// Requirements on Types:
+//     FDIter is a forward iterator type.
+//     The value type of FDIter has default ctor, dctor, copy=,
+//      operator<.
+//     operator< is a total order on the value type of FDIter.
+// Pre:
+//     [first, middle) and [middle, last) are valid ranges, each sorted
+//      by <.
+// Post:
+//     [first, last) contains the same items as it did initially, but
+//      now sorted by < (in a stable manner).
+template <typename FDIter>
+void stableMerge(FDIter first, FDIter middle, FDIter last)
+{
+    // ** C++03:
+    using Value = typename std::iterator_traits<FDIter>::value_type;
+    // ** C++11:
+//    using Value = typename remove_reference<decltype(*first)>::type;
+    // ** Is this really better?
+
+    std::vector<Value> buffer(distance(first, last));
+                                // Buffer for temporary copy of data
+    FDIter in1 = first;         // Read location in 1st half
+    FDIter in2 = middle;        // Read location in 2nd half
+    auto out = buffer.begin();  // Write location in buffer
+    // ** auto! That *is* better than vector<Value>::iterator
+
+    // Merge two sorted lists into a single list in buff.
+    while (in1 != middle && in2 != last)
+    {
+        if (*in2 < *in1)  // Must do comparison this way, to be stable.
+            *out++ = *in2++;
+        else
+            *out++ = *in1++;
+    }
+
+    // Copy remainder of original sequence to buffer.
+    // Only one of the following two copy calls will do anything, since
+    //  the other is given an empty source range.
+    copy(in1, middle, out);
+    copy(in2, last, out);
+
+    // Copy buffer contents back to original sequence location.
+    copy(buffer.begin(), buffer.end(), first);
+}
+
+
+// mergeSort
+// Sorts a sequence, using Merge Sort.
+// Recursive.
+// Requirements on Types:
+//     FDIter is a forward iterator type.
+//     The value type of FDIter has default ctor, dctor, copy=,
+//      operator<.
+//     operator< is a total order on the value type of FDIter.
+// Pre:
+//     [first, last) is a valid range.
+// Post:
+//     [first, last) contains the same items as it did initially,
+//      but now sorted by < (in a stable manner).
+template <typename FDIter>
+void mergeSort(FDIter first, FDIter last)
+{
+    // Compute size of sequence
+    std::size_t size = std::distance(first, last);
+
+    // BASE CASE
+    if (size <= 1)
+        return;
+
+    // RECURSIVE CASE
+    FDIter middle = first;
+    std::advance(middle, size/2);  // middle is iterator to middle of range
+
+    // Recursively sort the two lists
+    mergeSort(first, middle);
+    mergeSort(middle, last);
+
+    // And merge them
+    stableMerge(first, middle, last);
+}
+
+// ********************* MERGE_SORT.CPP ENDS *********************
 
 template<typename RAIter>
 std::size_t inversions(RAIter first, RAIter last){
+    mergeSort(first, last);
     return 0;
 }
 
